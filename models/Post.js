2c379f499b1ed3eb5f34dda0057eda4aec655ff7
Calvin Donner <calvinjdonner@gmail.com>
2022-01-08 17:20:53 -0700

    updates to styling and added rating

--- models/Post.js
+++ models/Post.js
@@ -3,36 +3,41 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
-Post.init(
-  {
+Post.init({
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
     title: {
-      type: DataTypes.STRING,
-      allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 0,
+            max: 100
+        }
     },
     post_text: {
-      type: DataTypes.STRING,
-      allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
     },
     user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
     }
-  },
-  {
+}, {
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'post'
-  }
-)
+})
 
 module.exports = Post;
